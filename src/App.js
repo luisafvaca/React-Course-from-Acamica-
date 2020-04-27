0ad6e0e9566a61d657c284127f844d821576d4ed
Luisa Vaca <luisafernanda9111@gmail.com>
2020-04-27 15:31:35 -0500

    basic use of styled components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components';
+import Proptypes from 'prop-types';
+
+const AppStyles = styled.button`
+  color: ${props => props.color ? props.color : 'red'};
+  border-color: tomato;
+  border-radius: 3px;
+`;
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+       <Container >
+        <AppStyles color='tomato'>
+          <h1>Hola Mundo</h1>
+        </AppStyles>
+      </Container>
     </div>
   );
+}
+
+AppStyles.Proptypes = {
+  color: Proptypes.string
 }
 
 export default App;
