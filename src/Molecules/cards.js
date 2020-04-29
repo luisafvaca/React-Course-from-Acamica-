b9fc26f23285372605456b62c67868fb84de6d9f
Luisa Vaca <luisafernanda9111@gmail.com>
2020-04-28 21:04:13 -0500

    added style

--- src/Molecules/cards.js
+++ src/Molecules/cards.js
@@ -4,7 +4,7 @@ import styled from 'styled-components';
 
 const CardsSection = styled.section`
     display: flex;
-    justify-content: center;
+    flex-flow: wrap;
 `
 class Cards extends React.Component {
     cards = (items) => {
